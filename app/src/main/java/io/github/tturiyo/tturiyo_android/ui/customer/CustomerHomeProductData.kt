ae832e092332f4bab1aca880eeca5d33305abf81
zzanzu <lees3301@gmail.com>
2018-05-22 22:06:17 +0900

    Add customer-home-product recyclerview's kotlin code

---
+++ app/src/main/java/io/github/tturiyo/tturiyo_android/ui/customer/CustomerHomeProductData.kt
@@ -0,0 +1,19 @@
+package io.github.tturiyo.tturiyo_android.ui.customer
+
+/**
+ * Created by user on 2018-05-22.
+ * companyImage - 가게 이미지
+ * companyName - 가게 이름
+ * productPriceBefore - 상품 정가, Int?
+ * productPriceAfter - 상품 할인가, Int?
+ * productSurplus - 상품 재고
+ * productLike - 상품 like 여부
+ */
+data class CustomerHomeProductData (
+        var companyImage : Int,
+        var companyName : String,
+        var productPriceBefore : String,
+        var productPriceAfter : String,
+        var productSurplus : String,
+        var productLike : Int
+)
