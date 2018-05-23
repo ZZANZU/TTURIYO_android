28f0e8b423b1205673380bc6ddd6782805037d2f
kujyp <pjo901018@gmail.com>
2018-05-23 21:25:30 +0900

    Add RxBinding as dependency / Add ProductData

---
+++ app/src/main/java/io/github/tturiyo/tturiyo_android/ui/seller/ProductData.kt
@@ -0,0 +1,9 @@
+package io.github.tturiyo.tturiyo_android.ui.seller
+
+object ProductData {
+    var companyname: String = ""
+    var companylocation: String = ""
+    var productname: String = ""
+    var productpriceafter: String = ""
+    var productdue: String = ""
+}
--- app/src/main/java/io/github/tturiyo/tturiyo_android/ui/seller/SellerActivity.kt
+++ app/src/main/java/io/github/tturiyo/tturiyo_android/ui/seller/SellerActivity.kt
@@ -2,14 +2,77 @@ package io.github.tturiyo.tturiyo_android.ui.seller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.jakewharton.rxbinding2.view.RxView
+import com.jakewharton.rxbinding2.widget.RxTextView
 import io.github.tturiyo.base.debug.Log
 import io.github.tturiyo.tturiyo_android.R
+import io.reactivex.Observable
+import io.reactivex.disposables.CompositeDisposable
+import kotlinx.android.synthetic.main.activity_seller.*
 
 class SellerActivity : AppCompatActivity() {
+    val disposables = CompositeDisposable()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_seller)
         Log.d()
+
+        initView()
+    }
+
+    private fun initView() {
+        Log.d()
+
+        disposables.addAll(
+                RxTextView.afterTextChangeEvents(et_companyname)
+                        .map { it.editable().toString() }
+                        .extLogd()
+                        .subscribe {
+                            ProductData.companyname = it
+                        },
+                RxTextView.afterTextChangeEvents(et_companylocation)
+                        .map { it.editable().toString() }
+                        .subscribe {
+                            ProductData.companylocation = it
+                        },
+                RxTextView.afterTextChangeEvents(et_productname)
+                        .map { it.editable().toString() }
+                        .subscribe {
+                            ProductData.productname = it
+                        },
+                RxTextView.afterTextChangeEvents(et_productpriceafter)
+                        .map { it.editable().toString() }
+                        .subscribe {
+                            ProductData.productpriceafter = it
+                        },
+                RxTextView.afterTextChangeEvents(et_productdue)
+                        .map { it.editable().toString() }
+                        .subscribe {
+                            ProductData.productdue = it
+                        }
+                )
+
+        disposables.add(
+                RxView.clicks(btn_submit)
+                        .subscribe {
+                            saveData()
+                        }
+        )
+    }
+
+    private fun saveData() {
+        Log.d()
+    }
+
+    override fun onDestroy() {
+        disposables.dispose()
+        super.onDestroy()
+    }
+}
+
+fun <T> Observable<T>.extLogd(): Observable<T> {
+    return this.doOnNext {
+        Log.d("data=$it")
     }
 }
