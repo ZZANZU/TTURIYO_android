031286503247e7159115663a1a0a22434021d8f5
kujyp <pjo901018@gmail.com>
2018-06-03 18:50:44 +0900

    Complete tracking current location

--- app/src/main/java/io/github/tturiyo/tturiyo_android/ui/customer/CustomerHomeSearchFragment.kt
+++ app/src/main/java/io/github/tturiyo/tturiyo_android/ui/customer/CustomerHomeSearchFragment.kt
@@ -1,6 +1,6 @@
 package io.github.tturiyo.tturiyo_android.ui.customer
 
-import android.graphics.Color
+import android.Manifest
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
@@ -8,12 +8,10 @@ import android.view.View
 import android.view.ViewGroup
 import io.github.tturiyo.base.debug.Log
 import io.github.tturiyo.tturiyo_android.R
-import io.github.tturiyo.tturiyo_android.managers.CustomLocationManager
+import io.github.tturiyo.tturiyo_android.managers.chkPermissions
 import io.reactivex.disposables.CompositeDisposable
 import kotlinx.android.synthetic.main.fragment_customer_home_search.view.*
-import net.daum.mf.map.api.MapCircle
 import net.daum.mf.map.api.MapPoint
-import net.daum.mf.map.api.MapPolyline
 import net.daum.mf.map.api.MapView
 
 
@@ -27,29 +25,73 @@ class CustomerHomeSearchFragment: Fragment() {
         }
     }
 
-    val disposables = CompositeDisposable()
+    private lateinit var mapView: MapView
+    private val disposables = CompositeDisposable()
+    private lateinit var inflatedView: View
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         Log.d("customer home search fragment created!")
-        val mView = inflater.inflate(R.layout.fragment_customer_home_search, container, false)
+        inflatedView = inflater.inflate(R.layout.fragment_customer_home_search, container, false)
 
-        val mapView = MapView(activity)
+        mapView = MapView(activity)
+        if (chkPermissions(Manifest.permission.ACCESS_FINE_LOCATION,
+                        Manifest.permission.ACCESS_COARSE_LOCATION)) {
+            Log.d("RequestLocationUpdates")
+            mapView.setCurrentLocationEventListener(object : MapView.CurrentLocationEventListener {
+                override fun onCurrentLocationUpdateFailed(p0: MapView?) {
+                    Log.d()
+                }
 
-        disposables.add(
-                CustomLocationManager.getLocationsAsObservable()
-                        .take(1)
-                        .subscribe {
-                            Log.d("First Location Event Location=$it")
-                            mapView.setMapCenterPoint(MapPoint.mapPointWithGeoCoord(it.latitude, it.longitude), true)
-                            mapView.currentLocationTrackingMode = MapView.CurrentLocationTrackingMode.TrackingModeOnWithMarkerHeadingWithoutMapMoving
-//                            mapView.setCurrentLocationRadius(100)
-//                            mapView.setCurrentLocationRadiusFillColor(Color.argb(77, 255, 255, 0))
-//                            mapView.setCurrentLocationRadiusStrokeColor(Color.argb(77, 255, 165, 0))
-                            mapView.setShowCurrentLocationMarker(true)
-                            mView.map_frame.addView(mapView)
-                        }
-        )
+                override fun onCurrentLocationUpdate(p0: MapView?, p1: MapPoint?, p2: Float) {
+                    Log.d()
+                    Log.d("latitude=${p1!!.mapPointGeoCoord.latitude}, longitude=${p1.mapPointGeoCoord.longitude}")
+                    if (mapView.currentLocationTrackingMode == MapView.CurrentLocationTrackingMode.TrackingModeOnWithoutHeading) {
+                        Log.d("mapView.currentLocationTrackingMode == MapView.CurrentLocationTrackingMode.TrackingModeOnWithoutHeading")
+                        mapView.setMapCenterPoint(MapPoint.mapPointWithGeoCoord(p1.mapPointGeoCoord.latitude, p1.mapPointGeoCoord.longitude), false)
+                        mapView.currentLocationTrackingMode = MapView.CurrentLocationTrackingMode.TrackingModeOnWithoutHeadingWithoutMapMoving
+                    }
+                }
 
-        return mView
+                override fun onCurrentLocationUpdateCancelled(p0: MapView?) {
+                    Log.d()
+                }
+
+                override fun onCurrentLocationDeviceHeadingUpdate(p0: MapView?, p1: Float) {
+                    Log.d()
+                }
+
+            })
+
+            mapView.currentLocationTrackingMode = MapView.CurrentLocationTrackingMode.TrackingModeOnWithoutHeading
+            inflatedView.map_frame.addView(mapView)
+        } else {
+            assert(false, { "권한이 없습니다." })
+        }
+
+        return inflatedView
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d()
+    }
+
+    override fun onDestroy() {
+        Log.d()
+        super.onDestroy()
+    }
+
+    override fun onDestroyView() {
+        Log.d()
+        mapView.currentLocationTrackingMode = MapView.CurrentLocationTrackingMode.TrackingModeOff
+        inflatedView.map_frame.removeView(mapView)
+        disposables.dispose()
+
+        super.onDestroyView()
     }
 }
